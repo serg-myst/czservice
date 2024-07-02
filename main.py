0e215aa015b581d17562e44a5c86940544d0af48
serg_myst <serg_myst@mail.ru>
2024-07-02 15:47:58 +0300

    token type add new column

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 import sys
 import jwt
 from sqlalchemy import delete, insert
-from config import SERIAL, URL_AUTH, URL_TOKEN, STORE
+from config import SERIAL, URL_AUTH, URL_TOKEN, STORE, URL_AUTH_SUZ, URL_TOKEN_SUZ, OMSID
 from config import LOGGER as log
 from models import Token
 from database import session_maker
@@ -14,8 +14,11 @@ from database import session_maker
 session = session_maker()
 
 
-def save_token(token_info: dict):
-    stmt = delete(Token)
+def save_token(token_info: dict, token_type: int):
+
+    stmp_type = 'CZ' if token_type == 0 else 'SUZ'
+
+    stmt = delete(Token).where(Token.token_type == stmp_type)
     session.execute(stmt)
     session.commit()
 
@@ -24,19 +27,29 @@ def save_token(token_info: dict):
     session.commit()
 
 
-def get_token_info(token: str, token_info: dict):
-    decoded = jwt.decode(token, options={"verify_signature": False})
-    offset = 10800  # +3 hours
-    expiration_date = datetime(1970, 1, 1) + timedelta(seconds=(int(decoded['exp'] + offset)))
+def get_token_info(token: str, token_info: dict, token_type: int):
+    if token_type == 0:
+        decoded = jwt.decode(token, options={"verify_signature": False})
+        offset = 10800  # +3 hours
+        expiration_date = datetime(1970, 1, 1) + timedelta(seconds=(int(decoded['exp'] + offset)))
 
-    token_info['expiration_date'] = expiration_date.isoformat(sep='T')
-    token_info['full_name'] = decoded['full_name']
-    token_info['mchd_id'] = str(decoded['mchd_id'])
-    token_info['inn'] = str(decoded['inn'])
+        token_info['token_type'] = 'CZ'
+        token_info['expiration_date'] = expiration_date.isoformat(sep='T')
+        token_info['full_name'] = decoded['full_name']
+        token_info['mchd_id'] = str(decoded['mchd_id'])
+        token_info['inn'] = str(decoded['inn'])
+    else:
+        offset = 32400
+        expiration_date = datetime.now() + timedelta(seconds=(offset))
+        token_info['token_type'] = 'SUZ'
+        token_info['expiration_date'] = expiration_date.isoformat(sep='T')
+        token_info['full_name'] = ''
+        token_info['mchd_id'] = ''
+        token_info['inn'] = ''
 
 
-def get_token():
-    log.info(f'CZ. Start - {datetime.now()}')
+def get_token(token_type: int):
+    log.info(f'CZ. Start - {datetime.now()}. TYPE {token_type}')
 
     CADES_BES = 1
     CAPICOM_ENCODE_BASE64 = 0
@@ -48,12 +61,17 @@ def get_token():
 
     headers = {'accept': 'application/json'}
 
-    response = requests.get(URL_AUTH, headers=headers)
+    if token_type == 0:
+        post_url = URL_AUTH
+    else:
+        post_url = URL_AUTH_SUZ
+
+    response = requests.get(post_url, headers=headers)
 
     status = response.status_code
 
     if status != 200:
-        log.error(f'Error. Status code {status}. URL {URL_AUTH}')
+        log.error(f'Error. Status code {status}. URL {URL_AUTH}. TYPE {token_type}')
         sys.exit()
 
     content = json.loads(response.content)
@@ -90,12 +108,17 @@ def get_token():
         'data': sSignedData
     }
 
-    response = requests.post(URL_TOKEN, headers=headers, json=params)
+    if token_type == 0:
+        post_url = URL_TOKEN
+    else:
+        post_url = f'{URL_TOKEN_SUZ}/{OMSID}'
+
+    response = requests.post(post_url, headers=headers, json=params)
 
     status = response.status_code
 
     if status != 200:
-        log.error(f'Error. Status code {status}. URL {URL_TOKEN}')
+        log.error(f'Error. Status code {status}. URL {post_url}. TYPE {token_type}')
         sys.exit()
 
     content = json.loads(response.content)
@@ -104,12 +127,14 @@ def get_token():
         'jwt_token': content['token']
     }
 
-    get_token_info(token_info['jwt_token'], token_info)
+    get_token_info(token_info['jwt_token'], token_info, token_type)
 
-    save_token(token_info)
+    save_token(token_info, token_type)
 
-    log.info(f'CZ. Stop -  {datetime.now()}')
+    log.info(f'CZ. Stop -  {datetime.now()}. TYPE {token_type}')
 
 
 if __name__ == '__main__':
-    get_token()
+    # 0 - cz token, 1 - suz token
+    get_token(0)
+    get_token(1)
